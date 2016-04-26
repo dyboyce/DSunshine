5a58c46ec54813bf149409dd4ae1d58e25a61f1c
Dylan Boyce <dylanboyce.206@gmail.com>
2016-04-26 09:43:21 -0700

    Initial commit

---
+++ app/src/main/java/dboyce/net/dsunshine/MainActivity.java
@@ -0,0 +1,22 @@
+package dboyce.net.dsunshine;
+
+import android.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public static class PlaceHolderFragment extends Fragment{
+
+
+
+    }
+
+}
